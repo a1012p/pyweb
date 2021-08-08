cd95268d694ebbf02acfb29eabcf1575b168b722
a1012p <68219393+a1012p@users.noreply.github.com>
2021-08-09 04:46:56 +0900

    데이터베이스

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,3 +3,10 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['a1021p.pythonanywhere.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
