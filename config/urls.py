4ddabd2b96b010ed7085e1e50ecc4f01d7828bab
a1012p <68219393+a1012p@users.noreply.github.com>
2021-08-09 06:11:31 +0900

    url 변경

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
 from django.views.static import serve
 
 from pybo.views import base_views
@@ -12,6 +12,7 @@ urlpatterns = [
     path('',base_views.index,name='index'),
     path('pybo/',include('pybo.urls')),
     path('common/',include('common.urls')),
-    url(r'^media/(?P<path>.\*)$', serve, {'document_root': settings.MEDIA_ROOT,}),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
