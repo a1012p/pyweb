baf4b69276b0bc6bc76bcfceea1a42c4accb1c65
a1012p <68219393+a1012p@users.noreply.github.com>
2021-08-09 05:38:01 +0900

    urls 디버그 if 문

--- config/urls.py
+++ config/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('pybo/',include('pybo.urls')),
     path('common/',include('common.urls')),
 ]
-urlpatterns +=  static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
