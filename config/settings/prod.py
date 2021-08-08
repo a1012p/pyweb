c36a2e24653cf18c4a2890f639034ab3079ae488
a1012p <68219393+a1012p@users.noreply.github.com>
2021-08-09 04:51:42 +0900

    인잇 추가

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,10 +3,3 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['a1021p.pythonanywhere.com']
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
