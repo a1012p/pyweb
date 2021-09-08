f06429a2fad3cb5f5a2b3211d56d51477ff1818e
a1012p <68219393+a1012p@users.noreply.github.com>
2021-09-08 14:09:41 +0900

    비밀번호 수정
    수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -149,6 +149,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT ='587'
 EMAIL_HOST_USER = 'a1027p@gmail.com'
-EMAIL_HOST_PASSWORD = 'ballday$@!!1245'
+EMAIL_HOST_PASSWORD = 'itotbhjuynnfaaas'
 EMAIL_USE_TLS = True
 
